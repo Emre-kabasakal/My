d15ee5a1a7fc4a1563682683cf40ae0b91af605e
Emre-kabasakal <130644451+Emre-kabasakal@users.noreply.github.com>
2023-04-15 06:38:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,27 +5,27 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578")) #لا تغير هاذة القيمة
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")#لا تغير هاذة القيمة
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-SESSION_NAME = getenv("SESSION_NAME", "")
+API_ID = int(getenv("API_ID", "25281981")) #لا تغير هاذة القيمة
+API_HASH = getenv("API_HASH", "b2787e463962d1eb306bb0608a188a83 تغير هاذة القيمة
+BOT_TOKEN = getenv("BOT_TOKEN", "6211334508:AAFvE9cTSif_G9Ep-e3OcAYd5JBV3fy9YMo")
+SESSION_NAME = getenv("SESSION_NAME", "BAGBxb0AhnkWT58xAiacTmOV-FJx0xqqOHyHcHxCaASdrbJ4Dn26XqllVISrFoG1I3NK8tBM7hw1PHG-HfPLKQ9nf9nsqQbZUdoedgHOjiKA006SNZW797klgFloULoxLQid3WJPyMSYsyzQTIBTj6aoFAzHxDZc77gE5ZRQ3PXtEdhZZaZXLLcaGGHRslHeflSTqTsj22QltpZqMEJws873PjR4-_ztRSB9D1wZ7Erj6g0aJqV_SuHFmqZb1ARZkGgla5KLrlYgZAwTclcscpWKFLDkcTpXgnWwk5wwhkTRM1IXoaWsvmjKlNSoYZn7rVysqQ5hpRR_Q9EX6YxJCmdLr8nGFwAAAAFshVwtAA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9") # @ هنا ضع يوزر حسابك بدون 
-ALIVE_NAME = getenv("ALIVE_NAME", "sonng") # هنا ضع اسم حسابك
-BOT_USERNAME = getenv("BOT_USERNAME", "") # @ هنا ضع يوزر البوت بدون 
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Emre_kabasakal") # @ هنا ضع يوزر حسابك بدون 
+ALIVE_NAME = getenv("ALIVE_NAME", "love_music") # هنا ضع اسم حسابك
+BOT_USERNAME = getenv("BOT_USERNAME", "Snow_White1_Bot") # @ هنا ضع يوزر البوت بدون 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR") 
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main") #لا تغير هاذة القيمة
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60")) #لا تغير هاذة القيمة
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9") # @ هنا ضغ يوزر كروبك بدون 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444") # @ هنا ضغ يوزر قناتك بدون
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Love00chaT") # @ هنا ضغ يوزر كروبك بدون 
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "WWWROROO") # @ هنا ضغ يوزر قناتك بدون
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1921025470").split()))
                                              #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1921025470").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
